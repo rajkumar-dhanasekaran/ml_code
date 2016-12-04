26f4c9bb74ac6b6d68efde8c23a70227ac99180e
rajkumar-dhanasekaran <rajkumar.dhanasekaran@gmail.com>
2016-12-04 18:50:40 +0530

    Added Hello world for Tensorflow
    Added Hello world for Tensorflow

--- helloworld_tensorflow.py
+++ helloworld_tensorflow.py
@@ -6,3 +6,24 @@ print(sess.run(hello))
 a = tf.constant(10)
 b = tf.constant(32)
 print(sess.run(a + b))
+
+#Another version
+# import tensorflow
+import tensorflow as tf
+
+# build computational graph
+a = tf.placeholder(tf.int16)
+b = tf.placeholder(tf.int16)
+
+addition = tf.add(a, b)
+
+# initialize variables
+init = tf.initialize_all_variables()
+
+# create session and run the graph
+with tf.Session() as sess:
+    sess.run(init)
+    print "Addition: %i" % sess.run(addition, feed_dict={a: 2, b: 3})
+
+# close session
+sess.close()
